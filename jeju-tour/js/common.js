8c1481e679ba0c0fc904a45859c5cec28d290067
jjungyoon06 <jjungyoon06@gmail.com>
2022-06-21 18:10:11 +0900

    pro

--- jeju-tour/js/common.js
+++ jeju-tour/js/common.js
@@ -1,12 +1,20 @@
+//모바일 메뉴 띄움
 $(document).ready(function(){
-
-    //모바일 메뉴 띄움
-    $('.openMOgnb').click(function(){
+        let isOpen = false; //상태 변수
+        $('.openMOgnb').click(function(){ //메뉴를 클릭하면       
+        if(isOpen == false){
         $('header').addClass('on');
         $('header .header_cont').slideDown('fast');
-        $('header .header_area .header_cont .closePop').show();			
+        $('header .header_area .header_cont .closePop').show();	   
+        isOpen = true; //상태 변경
+        }else{ //isOpen == true
+        $('.header_cont').slideUp('fast');
+        $('header').removeClass('on');
+        isOpen = false;
+        }
     });
     
+    //닫기 버튼 클릭
     $('header .header_cont .closePop').click(function(){
         $('.header_cont').slideUp('fast');
         $('header').removeClass('on');
@@ -16,7 +24,7 @@ $(document).ready(function(){
 //창을 늘리고 줄였을 때 메뉴바 설정
 $(function(){
     $(window).resize(function(){
-        if(parseInt($('header').css('width')>800)){
+        if(parseInt($('header').css('width'))>800){
             $('.header_cont').show();
         }else{
             $('.header_cont').hide();
